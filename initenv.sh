addad658fb730c252d5cece7dc4b711b8fcac34d
joel <joel@sfgr.nl>
2018-02-23 16:04:42 +0100

    copy paste in cmd

--- initenv.sh
+++ initenv.sh
@@ -1,5 +1,2 @@
- #!/bin/bash 
-
-source ~/Documents/CHI18-Paper/Zeeguu_virtenv/bin/activate
-export ZEEGUU_CORE_CONFIG=~/Documents/CHI18-Paper/Zeeguu-Core/zeeguu_chi.cfg 
+source ~/Documents/CHI18-Paper/Zeeguu_virtenv/bin/activate && export ZEEGUU_CORE_CONFIG=~/Documents/CHI18-Paper/Zeeguu-Core/zeeguu_chi.cfg
 
